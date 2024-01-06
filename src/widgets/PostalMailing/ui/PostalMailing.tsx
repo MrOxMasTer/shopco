c35deaa9ba57c8c392600fc02dfa4055fcdf109e
Aleksey <MrOxMasTer@users.noreply.github.com>
2024-01-06 16:49:55 +0300

    refactor: PostalMailing

--- src/widgets/PostalMailing/ui/PostalMailing.tsx
+++ src/widgets/PostalMailing/ui/PostalMailing.tsx
@@ -1,6 +1,14 @@
 import SvgMail from '@svg/mail.svg?icon';
 
 export const PostalMailing = () => {
+  const mailDelivery = async (formData: FormData) => {
+    'use server';
+
+    const email = formData.get('email');
+
+    console.log(email);
+  };
+
   return (
     <section>
       <div className="container py-[1.625rem]">
@@ -8,16 +16,20 @@ export const PostalMailing = () => {
           <h2 className="stn_title text-left text-white">
             Stay Upto Date About Our Latest Offers
           </h2>
-          <form>
+          <form action={mailDelivery}>
             <div className="mt-8 flex h-[42px] items-center gap-[14px] rounded-[62px] bg-white px-4 text-sm">
               <SvgMail className="flex-shrink-0" />
               <input
+                // name="email"
+                autoComplete="email"
                 type="email"
                 placeholder="Enter your email address"
                 className="w-full outline-none"
               />
             </div>
-            <button className="btn_lght mt-3 h-[42px] border-0 text-sm font-medium">
+            <button
+              type="submit"
+              className="btn_lght mt-3 h-[42px] border-0 text-sm font-medium">
               Subscribe to Newsletter
             </button>
           </form>
